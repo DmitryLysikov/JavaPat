c47ac6225df17a2339ed38e9020a4763869f8f0d
DmitryLysikov <129677338+DmitryLysikov@users.noreply.github.com>
2024-09-17 15:59:55 +0500

    Add files via upload

---
+++ To-Do-ListConsole/src/main/java/org/example/Task.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Task {
+    private static int idCounter = 0;
+    private int id;
+    private String title;
+    private String description;
+    private int priority;
+    private boolean status;
+    private LocalDate dueDate;
+
+    public Task(String title, String description, int priority, boolean status, LocalDate dueDate) {
+        this.dueDate = dueDate;
+        this.id = ++idCounter;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d, Title: %s, Due: %s, Priority: %d, Status: %b",
+                id, title, dueDate, priority, status);
+    }
+}
